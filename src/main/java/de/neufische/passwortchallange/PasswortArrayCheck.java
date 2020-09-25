05ffb8bf2a3dc9b227744275f083e3267953871a
Nils Mester <n-mester@mailbox.org>
2020-09-25 13:42:35 +0200

    Test

--- src/main/java/de/neufische/passwortchallange/PasswortArrayCheck.java
+++ src/main/java/de/neufische/passwortchallange/PasswortArrayCheck.java
@@ -4,6 +4,49 @@ public class PasswortArrayCheck {
 
     public static void main(String[] args) {
 
+        String [] passwords = {"1234", "BROTFÜRDIEWELT", "baumhaus", "Passwort123", "Jutesack"};
+
+        System.out.println(getStringsOutOfPasswordArray(passwords));
+
+        //System.out.println(passwords[2]);
+
+    }
+
+    public static boolean getStringsOutOfPasswordArray (String[] passwords){
+
+        for (int i = 0; i < passwords.length; i++){
+            String singlePassword = passwords[i];
+            boolean isLengthValid = checkPasswordLength(singlePassword);
+            boolean isUpperLowerValid = checkIfPasswordContainsUpperLowerCase(singlePassword);
+            boolean isNumberPresent = checkIfPasswordContainsNumber(singlePassword);
+
+            if (isLengthValid && isUpperLowerValid && isNumberPresent == true){
+                System.out.println(singlePassword);
+                System.out.println(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean checkPasswordLength (String password){
+        int minLength = 8;
+        int actualLength = password.length();
+        return actualLength >= minLength;
+    }
+
+    public static boolean checkIfPasswordContainsNumber (String password) {
+        return password.matches(".*\\d.*");
+    }
+
+    public static boolean checkIfPasswordContainsUpperLowerCase(String password) {
+        boolean hasUppercase = !password.equals(password.toLowerCase());
+        boolean hasLowercase = !password.equals(password.toUpperCase());
+        if(hasUppercase && hasLowercase == true) {
+            return true;
+        }
+        return false;
     }
 
 }
---
+++ src/test/java/de/neufische/passwortchallange/PasswortArrayCheckTest.java
@@ -0,0 +1,79 @@
+package de.neufische.passwortchallange;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswortArrayCheckTest {
+
+    @Test
+    void main() {
+
+    }
+
+    @Test
+    public void checkPasswordLongEnoughTest() {
+        String password = "Katzenhaus";
+
+        boolean isPasswordLongEnough = PasswortArrayCheck.checkPasswordLength(password);
+        assertTrue(isPasswordLongEnough);
+    }
+
+    @Test
+    public void checkPasswordTooShortTest() {
+        String password = "Katze";
+
+        boolean isPasswordTooShort = PasswortArrayCheck.checkPasswordLength(password);
+        assertFalse(isPasswordTooShort);
+    }
+
+    @Test
+    void checkIfPasswordContainsNumberTest() {
+        String password = "Katzenhaus1";
+
+        boolean PasswordContainsNumber = PasswortArrayCheck.checkIfPasswordContainsNumber(password);
+        assertTrue(PasswordContainsNumber);
+    }
+
+    @Test
+    void checkIfPasswordContainsNoNumberTest() {
+        String password = "Katzenhaus";
+
+        boolean PasswordContainsNoNumber = PasswortArrayCheck.checkIfPasswordContainsNumber(password);
+        assertFalse(PasswordContainsNoNumber);
+    }
+
+
+    /*@Test
+    void checkIfPasswordContainsUpperLowerCaseTest() {
+        String password = "Katzenhaus";
+
+        boolean passwordContainsUpperLowerCase = PasswortArrayCheck.checkIfPasswordContainsUpperLowerCase(password);
+        assertTrue(passwordContainsUpperLowerCase);
+
+    }*/
+
+    /*@Test
+    void checkIfPasswordContainsNoUpperLowerCaseTest() {
+        String password = "katzenhaus";
+
+        boolean passwordContainsNoUpperLowerCase = PasswortArrayCheck.checkIfPasswordContainsUpperLowerCase(password);
+        assertFalse(passwordContainsNoUpperLowerCase);
+    }*/
+
+    @ParameterizedTest(name = "password {0} should return {1}")
+    @CsvSource({
+            "Katzenhaus,true",
+            "katzenhaus, false",
+            "KATZENHAUS, false",
+    })
+    public void passwordContainsUpperLowerCase(String password, boolean check){
+        boolean UpperLowerCaseResult = PasswortArrayCheck.checkIfPasswordContainsUpperLowerCase(password);
+        assertEquals(check, UpperLowerCaseResult);
+    }
+
+
+
+}
