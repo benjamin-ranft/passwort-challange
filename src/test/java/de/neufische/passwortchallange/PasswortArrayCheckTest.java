3cf7a509ba5bb57daf51ceb32bfd2bc405a6a5a7
Benjamin Ranft <71656341+benjamin-ranft@users.noreply.github.com>
2020-10-01 16:59:46 +0200

    Fix Fail

--- src/test/java/de/neufische/passwortchallange/PasswortArrayCheckTest.java
+++ src/test/java/de/neufische/passwortchallange/PasswortArrayCheckTest.java
@@ -15,7 +15,7 @@ class PasswortArrayCheckTest {
 
     @Test
     public void checkPasswordLongEnoughTest() {
-        String password = "HUNDjdhfgedgdfgd";
+        String password = "Katzenhaus";
 
         boolean isPasswordLongEnough = PasswortArrayCheck.checkPasswordLength(password);
         assertTrue(isPasswordLongEnough);
